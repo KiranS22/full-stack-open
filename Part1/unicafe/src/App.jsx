0c80552f696f77a7a11fc86b0cd3488534fab549
Kiran <krnsundal@gmail.com>
2024-03-17 11:12:54 +0000

    completed 1.7

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,22 +1,54 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Display from "./Display";
 import Button from "./Button";
 
 const App = () => {
   const [good, setGood] = useState(0); // state for 'Good button'
-  const [neutral, setNeutral] = useState(0); //state for 'neutral' button
-  const [bad, setBad] = useState(0); //state for 'Bad button'
-  const handleGoodClick = () => setGood(good + 1);
-  const handleNeutralClick = () => setNeutral(neutral + 1);
-  const handleBadClick = () => setBad(bad + 1);
+  const [neutral, setNeutral] = useState(0); // state for 'neutral' button
+  const [bad, setBad] = useState(0); // state for 'Bad button'
+  const [total, setTotal] = useState(0); // state for total
+
+  // Function to calculate the average score
+  const calculateAvg = () => {
+    return total === 0 ? 0 : (good - bad) / total;
+  };
+
+  const percentOfPosFeedback = () => {
+    return total === 0 ? 0 : ((good / total) * 100).toFixed(8);
+  };
+
+  // Event handler for 'Good' button click
+  const handleGoodClick = () => {
+    setGood(good + 1); // Increment 'good' state
+    setTotal(total + 1); // Increment total
+  };
+
+  // Event handler for 'Neutral' button click
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1); // Increment 'neutral' state
+    setTotal(total + 1); // Increment total
+  };
+
+  // Event handler for 'Bad' button click
+  const handleBadClick = () => {
+    setBad(bad + 1); // Increment 'bad' state
+    setTotal(total + 1); // Increment total
+  };
 
   return (
     <>
-      <h1>give feedback</h1>
-      <Button onSmash={handleGoodClick} text="Good"></Button>
-      <Button onSmash={handleNeutralClick} text="Neutral"></Button>
-      <Button onSmash={handleBadClick} text="bad"></Button>
-      <Display good={good} neutral={neutral} bad={bad} />
+      <h1>Give feedback</h1>
+      <Button onSmash={handleGoodClick} text="Good" />
+      <Button onSmash={handleNeutralClick} text="Neutral" />
+      <Button onSmash={handleBadClick} text="Bad" />
+      <Display
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        avg={calculateAvg()}
+        percent={percentOfPosFeedback()}
+      />
     </>
   );
 };
--- Part1/unicafe/src/Display.jsx
+++ Part1/unicafe/src/Display.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-const Display = ({ good, neutral, bad }) => {
+const Display = ({ good, neutral, bad, total, avg, percent }) => {
   return (
     <div>
       <h2>feedback results</h2>
       <p>Good: {good} </p>
       <p>Neutral:{neutral} </p>
       <p>Bad:{bad} </p>
+      <p>feedback total: {total} </p>
+      <p>avg num of responses: {avg}</p>
+      <p>Percentage of positive feedback:{percent}%</p>
     </div>
   );
 };
